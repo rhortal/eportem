57fd9bc521de8302543caadc5c80eb40a876a323
Roberto Hortal <rhortal@gmail.com>
2023-05-26 13:58:18 +0200

    Fixed naming error

---
+++ utility/telegram_send.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+# Define function to send message
+def send_telegram_message(message_text):
+    # Get the directory of the current file
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Construct the file path
+    file_path = os.path.join(current_dir, '../config/telegram.txt')
+
+    # Read the Telegram parameters
+    with open(file_path, "r") as f:
+        token, userID = f.read().splitlines()
+
+    # Create url
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+
+    # Create json link with message
+    data = {'chat_id': userID, 'text': message_text}
+
+    # POST the message
+    requests.post(url, data)
+
+# Example usage
+if __name__ == '__main__':
+    send_telegram_message("Hello from my function!")
