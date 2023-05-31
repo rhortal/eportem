2f413b5ed200a3443e92ae8e144b59bfde45815a
Roberto Hortal <rhortal@gmail.com>
2023-05-31 18:23:44 +0200

    Implemented config via .env

--- utility/telegram_send.py
+++ utility/telegram_send.py
@@ -1,17 +1,20 @@
 import requests
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 # Define function to send message
 def send_telegram_message(message_text):
     # Get the directory of the current file
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    # Construct the file path
-    file_path = os.path.join(current_dir, '../config/telegram.txt')
+    # Load the .env file
+    dotenv_path = Path(os.path.join(current_dir, '../config/.env'))
+    load_dotenv(dotenv_path=dotenv_path)
 
-    # Read the Telegram parameters
-    with open(file_path, "r") as f:
-        send, token, userID = f.read().splitlines()
+    send = os.getenv('TELEGRAM_NOTIFY')
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    userID = os.getenv('TELEGRAM_CHAT_ID')
 
     if send == "YES":
         # Create url
